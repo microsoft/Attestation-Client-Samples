7f2dd602eb790de6db6fe64d2b42cf5eaa6c0725
Lester Cheng <lester.cs.cheng@gmail.com>
2023-08-28 16:28:02 -0500

    Roll back changes to f278c2c1

--- attestation/utils.h
+++ attestation/utils.h
@@ -17,7 +17,4 @@ wil::unique_ncrypt_key create_tpm_key(const std::wstring& name, bool machine_key
 // Returns the value of an environment variable.
 std::string get_env_var(const std::string& env);
 
-// Returns the value of an environment variable or a default string
-std::string get_env_var(const std::string& env, const std::string def);
-
 #endif // _ATT_SAMPLES_UTILS_H
