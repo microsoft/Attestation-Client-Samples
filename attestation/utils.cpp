2cd087751a0b3fbca06905d36cce60e09b8a3029
Lester Cheng <lestercheng@microsoft.com>
2024-05-03 13:15:07 -0500

    Add new sample with vbs ncrypt

--- attestation/utils.cpp
+++ attestation/utils.cpp
@@ -28,20 +28,90 @@ wil::unique_ncrypt_key create_tpm_key(const wstring& name, bool machine_key)
 {
     cout << "Creating TPM key...";
 
-    wil::unique_ncrypt_prov tpm_prov{};
-    THROW_IF_FAILED_MSG(NCryptOpenStorageProvider(&tpm_prov, MS_PLATFORM_KEY_STORAGE_PROVIDER, 0), "Error opening TPM provider.");
-
-    wil::unique_ncrypt_key tpm_key{};
-    THROW_IF_FAILED_MSG(NCryptCreatePersistedKey(tpm_prov.get(), &tpm_key, BCRYPT_RSA_ALGORITHM, name.c_str(), 0, NCRYPT_OVERWRITE_KEY_FLAG | (machine_key ? NCRYPT_MACHINE_KEY_FLAG : 0)), "Error creating TPM key.");
-
-    DWORD key_len = 2048;
-    THROW_IF_FAILED_MSG(NCryptSetProperty(tpm_key.get(), NCRYPT_LENGTH_PROPERTY, reinterpret_cast<PBYTE>(&key_len), sizeof(key_len), 0), "Error setting key length.");
+    auto tpm_key = create_key(MS_PLATFORM_KEY_STORAGE_PROVIDER, name, NCRYPT_OVERWRITE_KEY_FLAG | (machine_key ? NCRYPT_MACHINE_KEY_FLAG : 0), false);
 
     THROW_IF_FAILED_MSG(NCryptFinalizeKey(tpm_key.get(), 0), "Error finalizing TPM key.");
 
     cout << " Done." << endl;
 
     return tpm_key;
+}
+
+wil::unique_ncrypt_key create_key(PCWSTR providerName, const wstring& keyName, DWORD flags, bool finalize)
+{
+    wil::unique_ncrypt_prov provHandle{};
+    THROW_IF_FAILED_MSG(NCryptOpenStorageProvider(
+        &provHandle,
+        providerName,
+        0), "NCryptOpenStorageProvider failed.");
+
+    wil::unique_ncrypt_key key{};
+    THROW_IF_FAILED_MSG(NCryptCreatePersistedKey(
+        provHandle.get(),
+        &key,
+        BCRYPT_RSA_ALGORITHM,
+        keyName.c_str(),
+        0,
+        flags), "NCryptCreatePersistedKey failed.");
+
+    DWORD key_len = 2048;
+    THROW_IF_FAILED_MSG(
+        NCryptSetProperty(
+            key.get(),
+            NCRYPT_LENGTH_PROPERTY,
+            reinterpret_cast<PBYTE>(&key_len),
+            sizeof(key_len),
+            0), "NCryptSetProperty for key length failed.");
+
+    if (finalize)
+    {
+        THROW_IF_FAILED_MSG(NCryptFinalizeKey(key.get(), 0), "NCryptFinalizeKey failed.");
+    }
+
+    return key;
+}
+
+bool CanCreateKeyGuardKey()
+{
+    // bool result = false;
+
+    SECURITY_STATUS status;
+    NCRYPT_PROV_HANDLE hProv = 0;
+    NCRYPT_KEY_HANDLE hKey = 0;
+
+    status = NCryptOpenStorageProvider(&hProv, MS_KEY_STORAGE_PROVIDER, 0);
+    if (status != ERROR_SUCCESS)
+    {
+        wprintf(L"NCryptOpenStorageProvider failed with %x\n", status);
+        if (hKey)
+        {
+            NCryptFreeObject(hKey);
+        }
+
+        if (hProv)
+        {
+            NCryptFreeObject(hProv);
+        }
+        return false;
+    }
+
+    status = NCryptCreatePersistedKey(hProv, &hKey, NCRYPT_ECDSA_P384_ALGORITHM, NULL, 0, NCRYPT_USE_VIRTUAL_ISOLATION_FLAG);
+    if (status != ERROR_SUCCESS)
+    {
+        wprintf(L"NCryptCreatePersistedKey failed with %x\n", status);
+        if (hKey)
+        {
+            NCryptFreeObject(hKey);
+        }
+
+        if (hProv)
+        {
+            NCryptFreeObject(hProv);
+        }
+        return false;
+    }
+
+    return true;
 }
 
 std::string get_env_var(const std::string& env)
--- attestation/utils.h
+++ attestation/utils.h
@@ -14,6 +14,12 @@ wil::unique_ncrypt_key load_tpm_key(const std::wstring& name, bool machine_key);
 // Creates a 2048-bit RSA key in the TPM using the Platform Key Storage Provider.
 wil::unique_ncrypt_key create_tpm_key(const std::wstring& name, bool machine_key);
 
+// Creates a 2048-bit RSA key
+wil::unique_ncrypt_key create_key(PCWSTR providerName, const std::wstring& keyName, DWORD flags, bool finalize);
+
+// Returns true if a Key Guard key can be created.
+bool CanCreateKeyGuardKey();
+
 // Returns the value of an environment variable.
 std::string get_env_var(const std::string& env);
 
