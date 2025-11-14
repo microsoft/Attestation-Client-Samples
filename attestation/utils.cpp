80ee94dc94e910ad40466e51f7404b4ff92bc1cc
Caroline Creamer <cacreamer@microsoft.com>
2025-11-13 16:48:13 -0800

    fix spacing

--- attestation/utils.cpp
+++ attestation/utils.cpp
@@ -27,29 +27,13 @@ wil::unique_ncrypt_key load_tpm_key(const wstring& name, bool machine_key)
 wil::unique_ncrypt_key create_key(PCWSTR provider_name, PCWSTR key_name, DWORD flags)
 {
     wil::unique_ncrypt_prov provider_handle{};
-    THROW_IF_FAILED_MSG(NCryptOpenStorageProvider(
-        &provider_handle,
-        provider_name,
-        0), "Error opening storage provider.");
+    THROW_IF_FAILED_MSG(NCryptOpenStorageProvider(&provider_handle, provider_name, 0), "Error opening storage provider.");
 
     wil::unique_ncrypt_key key{};
-    THROW_IF_FAILED_MSG(NCryptCreatePersistedKey(
-        provider_handle.get(),
-        &key,
-        BCRYPT_RSA_ALGORITHM,
-        key_name,
-        0,
-        flags), "Error creating key.");
+    THROW_IF_FAILED_MSG(NCryptCreatePersistedKey(provider_handle.get(), &key, BCRYPT_RSA_ALGORITHM, key_name, 0, flags), "Error creating key.");
 
     DWORD key_len = 2048;
-    THROW_IF_FAILED_MSG(
-        NCryptSetProperty(
-            key.get(),
-            NCRYPT_LENGTH_PROPERTY,
-            reinterpret_cast<PBYTE>(&key_len),
-            sizeof(key_len),
-            0), "Error setting key length.");
-
+    THROW_IF_FAILED_MSG(NCryptSetProperty(key.get(), NCRYPT_LENGTH_PROPERTY, reinterpret_cast<PBYTE>(&key_len), sizeof(key_len), 0), "Error setting key length.");
 
     THROW_IF_FAILED_MSG(NCryptFinalizeKey(key.get(), 0), "Error finalizing key.");
 
@@ -60,11 +44,7 @@ wil::unique_ncrypt_key create_tpm_key(const PCWSTR& name, bool machine_key)
 {
     cout << "Creating TPM key...";
 
-    wil::unique_ncrypt_key tpm_key = create_key(
-        MS_PLATFORM_KEY_STORAGE_PROVIDER,
-        name,
-        NCRYPT_OVERWRITE_KEY_FLAG | (machine_key ? NCRYPT_MACHINE_KEY_FLAG : 0)
-    );
+    wil::unique_ncrypt_key tpm_key = create_key(MS_PLATFORM_KEY_STORAGE_PROVIDER, name, NCRYPT_OVERWRITE_KEY_FLAG | (machine_key ? NCRYPT_MACHINE_KEY_FLAG : 0));
 
     cout << " Done." << endl;
 
@@ -75,11 +55,7 @@ wil::unique_ncrypt_key create_ephemeral_software_key()
 {
     cout << "Creating ephemeral software key...";
 
-    wil::unique_ncrypt_key ephemeral_software_key = create_key(
-        MS_KEY_STORAGE_PROVIDER,
-        NULL,
-        0
-    );
+    wil::unique_ncrypt_key ephemeral_software_key = create_key(MS_KEY_STORAGE_PROVIDER, NULL, 0);
 
     cout << " Done." << endl; 
     
@@ -123,14 +99,8 @@ LPVOID create_enclave()
     };
 
     // Customize the enclave parameters as needed for your application.
-    LPVOID enclave_base = CreateEnclave(GetCurrentProcess(),
-        0,
-        0x10000000, // Enclave size MUST match configuration in enclave.c.
-        0,
-        ENCLAVE_TYPE_VBS,
-        &create_info,
-        sizeof(create_info),
-        nullptr);
+    // Enclave size MUST match configuration in enclave.c. 
+    LPVOID enclave_base = CreateEnclave(GetCurrentProcess(), 0, 0x10000000, 0, ENCLAVE_TYPE_VBS, &create_info, sizeof(create_info), nullptr);
 
     THROW_LAST_ERROR_IF_NULL(enclave_base);
 
@@ -151,11 +121,7 @@ LPVOID create_enclave()
         1                    // thread_count
     };
 
-    THROW_IF_WIN32_BOOL_FALSE(InitializeEnclave(GetCurrentProcess(),
-        enclave_base,
-        &init_info,
-        init_info.Length,
-        nullptr));
+    THROW_IF_WIN32_BOOL_FALSE(InitializeEnclave(GetCurrentProcess(), enclave_base, &init_info, init_info.Length, nullptr));
 
     return enclave_base;
 }
